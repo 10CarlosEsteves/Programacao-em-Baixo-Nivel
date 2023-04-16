c0890ef8c0823d2f031a8d3871602c35ffa5ca65
Carlos Henrique Miranda Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2023-04-15 21:24:24 -0300

    Update main.cpp

--- C/Estrutura de Dados/HospitalDeque/main.cpp
+++ C/Estrutura de Dados/HospitalDeque/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+#include "Patient.hpp"
+
+using namespace std;
+
+
+int main(){
+	
+	Patient paci1('M', "Tess Teiges", 1001, NULL), paci2('M', "Aleglande de Moraes", 1002, NULL);
+	
+
+	
+	
+	
+	
+	return 0;
+}
